bed225a61fae8a44ebb5df47502216093e9178b4
kunanzheng <121325227+kunanzheng@users.noreply.github.com>
2023-01-02 18:28:57 +0800

    Add files via upload

---
+++ Xiangteng-Sima/appStoreSystem/src/readme.java
@@ -0,0 +1,8 @@
+/*
+Name: Xiangteng Sima
+StudentID: 202283930002
+Class: Software Engineering Class 1
+Description: This assignment through the parent class, subclass and inheritance knowledge to achieve the app management function.
+At the same time, I found that the traditional string method would judge the wrong situation without this type of apps, so I adopted
+the StringBuilder method after asking my classmate.
+* */
