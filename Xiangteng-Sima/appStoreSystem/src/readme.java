0abf0e81f8d2042687bc7f489460ea54c10dfb71
kunanzheng <121325227+kunanzheng@users.noreply.github.com>
2023-01-02 18:36:14 +0800

    Update readme.java

--- Xiangteng-Sima/appStoreSystem/src/readme.java
+++ Xiangteng-Sima/appStoreSystem/src/readme.java
@@ -4,5 +4,6 @@ StudentID: 202283930002
 Class: Software Engineering Class 1
 Description: This assignment through the parent class, subclass and inheritance knowledge to achieve the app management function.
 At the same time, I found that the traditional string method would judge the wrong situation without this type of apps, so I adopted
-the StringBuilder method after asking my classmate.
-* */
+the StringBuilder method after asking my classmate. This is my first time using Github to upload a project, so please bear with me
+if there is anything inappropriate.
+*/
